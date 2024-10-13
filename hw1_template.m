437a500973049b40248008ade4b750cb08acdc3c
Türker Dolapçı <dolapci.turker@gmail.com>
2024-10-14 00:31:08 +0300

    Create hw1_template.m

---
+++ hw1_template.m
@@ -0,0 +1,62 @@
+%% Information
+%Title of the script
+
+%Short information on the script (optional)
+
+%Your name
+
+%Your student ID number
+
+%Date
+
+
+%% Make a fresh start
+%Clear all workspace
+
+%Close all windows
+
+%Clear command window
+
+
+%% Input section
+%Define frequency of the signals to be generated
+
+%Define sampling rate of the signals to be generated
+
+%Define time durations for dot and dash
+
+%Define time durations for different silence periods 
+% (intra-letter, inter letter, inter word)
+
+
+%% Generate time vectors to generate dit, dash and different length silence vectors
+
+
+%% Generate dit and dash vectors
+
+
+%% Generate silence vectors
+
+
+%% Generate required letters (letters in your name) by concatenation of dit, dash and, silence with proper order
+
+
+%% Generate word (your name) by concatenation of letters with proper order
+
+
+%% Plot generated result vs time
+%Create a new figure
+
+%Plot the graph
+
+%Add title, axis titles, grid
+
+
+%% Save the figure as .png
+
+
+%% Play the sound of generated result
+
+
+%% Save the sound as .wav
+
